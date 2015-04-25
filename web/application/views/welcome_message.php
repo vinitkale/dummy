a384d116140e94ab90414229395340abd585ee97
ankita <ankita.panse@gmail.com>
2015-04-25 19:22:13 +0530

    test

--- web/application/views/welcome_message.php
+++ web/application/views/welcome_message.php
@@ -71,16 +71,25 @@
         <div id="container">
             <div id="fb-root"></div>
             <script>
+                window.fbAsyncInit = function () {
+                    FB.init({
+                        appId: '839785072768900',
+                        xfbml: true,
+                        version: 'v2.3'
+                    });
+                };
+
                 (function (d, s, id) {
                     var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id))
+                    if (d.getElementById(id)) {
                         return;
+                    }
                     js = d.createElement(s);
                     js.id = id;
-                    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=839785072768900";
+                    js.src = "//connect.facebook.net/en_US/sdk.js";
                     fjs.parentNode.insertBefore(js, fjs);
                 }(document, 'script', 'facebook-jssdk'));
-
+                
                 function checkLoginState() {
                     FB.api('/me', function (response) {
                         console.log(JSON.stringify(response));
