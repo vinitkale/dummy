51576cab7ff546c3eb49f93d2f49e410367325bb
ankita <ankita.panse@gmail.com>
2015-04-25 19:56:34 +0530

    fb

--- web/application/views/welcome_message.php
+++ web/application/views/welcome_message.php
@@ -69,7 +69,7 @@
         <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-
+        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
